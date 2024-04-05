930711ec503e7f5918ecec81048a9472ae2e5fdc
Iris Andrussow <andrussow@is.mpg.de>
2024-04-05 14:10:54 +0200

    Timing

--- trifinger_rl_example/example.py
+++ trifinger_rl_example/example.py
@@ -8,6 +8,62 @@ from trifinger_rl_datasets import PolicyBase, PolicyConfig
 from . import policies
 
 
+class NoHapticsPolicy(PolicyBase):
+
+    _goal_order = ["object_keypoints", "object_position", "object_orientation"]
+
+    def __init__(
+        self,
+        action_space,
+        observation_space,
+        episode_length,
+    ):
+        torch_model_path = "/is/sg2/iandrussow/training_results/2024_03_26_forcemap/crr/working_directories/0/policy.pt"
+        self.action_space = action_space
+        self.device = "cuda"
+        self.dtype = np.float32
+
+        # load torch script
+        self.policy = torch.jit.load(
+            torch_model_path, map_location=torch.device(self.device)
+        )
+
+    @staticmethod
+    def get_policy_config():
+        return PolicyConfig(
+            flatten_obs=True,
+            image_obs=False,
+        )
+
+    def reset(self):
+        pass  # nothing to do here
+
+    def get_action(self, observation):
+
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+
+        obs = np.concatenate(
+            (
+                observation["robot_information"],
+                np.array(observation["haptic_information"]["force_maps"]).flatten(),
+            ),
+            axis=0,
+        )
+        obs = torch.tensor(obs, dtype=torch.float, device=self.device)
+
+        action = self.policy(obs.unsqueeze(0))
+        action = action.detach().numpy()[0]
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+
+        end.record()
+        torch.cuda.synchronize()
+        print(start.elapsed_time(end))
+
+        return action
+
+
 class ForceMapPolicy(PolicyBase):
 
     _goal_order = ["object_keypoints", "object_position", "object_orientation"]
@@ -20,7 +76,7 @@ class ForceMapPolicy(PolicyBase):
     ):
         torch_model_path = "/is/sg2/iandrussow/training_results/2024_03_26_forcemap/crr/working_directories/0/policy.pt"
         self.action_space = action_space
-        self.device = "cpu"
+        self.device = "cuda"
         self.dtype = np.float32
 
         # load torch script
