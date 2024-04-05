0ec0a3aa4c875ae618040bf8b87b580e4d740ed6
Iris Andrussow <andrussow@is.mpg.de>
2024-04-05 15:33:47 +0200

    olicy

--- trifinger_rl_example/example.py
+++ trifinger_rl_example/example.py
@@ -71,9 +71,10 @@ class ForceMapPolicy(PolicyBase):
         observation_space,
         episode_length,
     ):
+        print("CUDA: ", torch.cuda.is_available())
         torch_model_path = "/is/sg2/iandrussow/training_results/2024_03_26_forcemap/crr/working_directories/0/policy.pt"
         self.action_space = action_space
-        self.device = "cpu"
+        self.device = "cuda"
         self.dtype = np.float32
 
         # load torch script
