1b0473dea4c9beb4cd26da9b86ce2b036f862231
Iris Andrussow <andrussow@is.mpg.de>
2024-04-10 17:58:12 +0200

    Half precision

--- trifinger_rl_example/example.py
+++ trifinger_rl_example/example.py
@@ -75,13 +75,14 @@ class ForceMapPolicy(PolicyBase):
         torch_model_path = "/is/sg2/iandrussow/training_results/2024_03_26_forcemap/crr/working_directories/0/policy.pt"
         # torch_model_path = "/home/andrussow/cluster/snagi/training_results/2024_03_26_forcemap/crr/working_directories/0/policy.pt"
         self.action_space = action_space
-        self.device = "cuda"
+        self.device = "gpu"
         self.dtype = np.float32
 
         # load torch script
         self.policy = torch.jit.load(
             torch_model_path, map_location=torch.device(self.device)
         )
+        self.policy.to(torch.half)
         self.timings = []
 
     @staticmethod
@@ -91,13 +92,16 @@ class ForceMapPolicy(PolicyBase):
             image_obs=False,
         )
 
-    def reset(self):
+    def get_timing(self):
         if len(self.timings) > 0:
             print(
                 "Mean timing of inference in the last episode: ",
                 sum(self.timings) / len(self.timings),
             )
             self.timings = []
+
+    def reset(self):
+        pass
 
     def get_action(self, observation):
 
@@ -108,7 +112,7 @@ class ForceMapPolicy(PolicyBase):
             ),
             axis=0,
         )
-        obs = torch.tensor(obs, dtype=torch.float, device=self.device)
+        obs = torch.tensor(obs, dtype=torch.half, device=self.device)
 
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
