93c8df53d68ca3f8a29b6486e0cea09269f92531
Iris Andrussow <andrussow@is.mpg.de>
2024-04-29 14:53:55 +0200

    No cudnn benchmarking

--- trifinger_rl_example/example.py
+++ trifinger_rl_example/example.py
@@ -12,7 +12,7 @@ import onnxruntime as ort
 
 logging.basicConfig(level=logging.INFO)
 
-torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.benchmark = True
 
 
 class NoHapticsPolicy(PolicyBase):
